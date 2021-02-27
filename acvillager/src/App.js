def8ad19f185f4db78e3fb7e358d6f4dbf273193
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2021-02-26 21:10:03 -0600

    added state
    added get request and required headers. villagers console log.

--- acvillager/src/App.js
+++ acvillager/src/App.js
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import AppHeader from './components/Header';
 import './App.css';
-import AppHeader from './Header';
 
 function App() {
+
+  const [villagers, setVillagers] = useState([])
+
+  useEffect(() => { 
+
+    // required headers for using Nookpedia API
+    const config = { headers: 
+      {'X-API-KEY': `${process.env.REACT_APP_NOOKAPIKEY}`,
+      'Accept-Version': '1.0.0'}
+    }
+
+    axios
+      .get(`https://api.nookipedia.com/villagers`, config)
+      .then(res => setVillagers(res.data))
+      .catch(error => console.log(error));
+
+
+  }, [])
+
   return (
     <div className="App">
         <AppHeader />
