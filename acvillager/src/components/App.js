7a89f04db125177545779ff4f53249cf90006a31
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2021-02-26 18:06:00 -0600

    created header
    created header component, imported img (displaying). started set background color for app.

--- acvillager/src/components/App.js
+++ acvillager/src/components/App.js
@@ -1,11 +1,10 @@
 import './App.css';
+import AppHeader from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>AC Villager Project</p>
-      </header>
+        <AppHeader />
     </div>
   );
 }
