8a79f4ca18e3ac5e8ac967c3b667a163ff6da820
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2021-03-05 15:21:03 -0600

    added dynamic search bar. Ran into conflict with pagination.

--- acvillager/src/components/villagers/VillagersCollection.js
+++ acvillager/src/components/villagers/VillagersCollection.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SingleVillagerCard from './SingleVillagerCard'
 
-function VillagersCollection({ villagers, loading }) {
+function VillagersCollection({ pureVillagerArray, villagers, loading }) {
+
+    const [searchVillager, setSearchVillager] = useState('')
 
     if (loading) {
         return <h3>loading...</h3>
     }
 
     return (
-        <section className="villager-list">
-            {villagers.map(villager => (
-                <SingleVillagerCard key={villager.name} villager={villager}>
-                </SingleVillagerCard>
-            ))}
-        </section>
+        <div>
+            <input type="text" placeholder="villager name..." name="villager-search" onChange={(event) => {
+                setSearchVillager(event.target.value)
+                console.log(event.target.value)
+            }} />
+            <section className="villager-list">
+                {villagers.filter((character) => {
+                    if (searchVillager == "") {
+                        return character
+                    } else if (character.name.toLowerCase().includes(searchVillager.toLowerCase())) {
+                        return character
+                    }
+                }).map(villager => (
+                    <SingleVillagerCard key={villager.name} villager={villager}>
+                    </SingleVillagerCard>
+                ))}
+            </section>
+        </div>
     )
 }
 
