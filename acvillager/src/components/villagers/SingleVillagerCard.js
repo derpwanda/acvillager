36ae1339d0983d3d2d784e9a4b581a45f74f18cc
Wanda Evans <39149409+derpwanda@users.noreply.github.com>
2021-03-06 14:28:16 -0600

    updated src url string

--- acvillager/src/components/villagers/SingleVillagerCard.js
+++ acvillager/src/components/villagers/SingleVillagerCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function SingleVillagerCard({ villager }) {
+    // "https://dodo.ac/np/images/8/85/June_NH.png"
+    // "https://dodo.ac/np/images/thumb/8/85/June_NH.png/40px-June_NH.png"
+
+    let characterURL = villager.image_url
+    // console.log(characterURL)
+
     return (
         <div className="single-villager">
-            <img src={villager.image_url} alt='villager'></img>
+            <img src={characterURL} alt='villager'></img>
             <div>{villager.name}</div>
         </div>
     )
